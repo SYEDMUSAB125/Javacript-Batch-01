0832ceb11a0a0b14ba358b1f55ae1ce729a01ee6
Syed Musab <syedmasab125@gmail.com>
2023-12-17 19:25:33 +0500

    first class of javascript

---
+++ app.js
@@ -0,0 +1,59 @@
+// Data types
+// String
+// Number
+// Boolean
+// undefined or null
+//  Numbers
+// var name = 8
+// console.log(name);
+
+// String
+// var name = "mark zingerburgerlidy89aseydhosifye89fsdklvhdifyh"
+// console.log(name)
+
+// high level low level 
+// boolean
+// var name = false;
+// console.log(name)
+// declare assigned
+
+// undefine 
+// var name;
+// console.log(name)
+// redeclare 
+// var name = "arham";
+// var name = "ahmed";
+// console.log(name)
+// console.log(name)
+//  const can not redeclare
+// const name ="musab";
+// const name ="aqib"
+// variable leagal and illegal
+// var na me = "musab"
+// var name1
+// var $name
+// var firstname;
+// var _nmae;
+
+// var name;
+// fruotfull name for best practice
+// var name="arham";
+// var name1="iqbal";
+// camel case 
+
+// var firstName= "arham";
+// var lastName="iqbal";
+
+// case sensitive
+// var rose=1;
+// var Rose=2;
+// console.log(rose);
+// console.log(Rose);
+
+
+
+
+
+
+
+
