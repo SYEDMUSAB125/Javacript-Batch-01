135834317a53652c0083ab98b910d9bf5c022f14
Syed Musab <syedmasab125@gmail.com>
2024-01-08 22:32:53 +0500

    class7.js uploaded

---
+++ class7.js
@@ -0,0 +1,54 @@
+// var name = ["m","u","s","a","b"] iteration on array
+var names = ["m","u","s","a","b"]
+for(var i =0; i<names.length; i++){
+   var c = c + " " + names[i] 
+  
+}
+console.log(c)
+// musab
+
+// function
+// function sum(){
+//     var a = 3;
+//     var b =3;
+//     var c =  a+b;
+//     console.log(c)
+// } 
+
+// sum();
+// function Minus(){
+//     var a = 3;
+//         var b =3;
+//         var c =  a-b;
+//         console.log(c)
+// }
+// Minus();
+//              paramter
+// function sum(a,b){
+//      var c =  a+b;
+//     console.log(c)
+// }
+// sum(2,3)  arguments
+
+// task
+// Greet 
+// hi! Musab How are you?
+// var n = prompt("Enter your Name");
+// function Greet(name){
+//     console.log("asssalam o alaikum "+name+ " How are you?" )
+// }
+// Greet(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
