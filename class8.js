c3e058a6777b0d926361a2eb3e837c383e4175c7
Syed Musab <syedmasab125@gmail.com>
2024-01-14 18:55:03 +0500

    class 8 uploaded

---
+++ class8.js
@@ -0,0 +1,46 @@
+// var num = prompt("Enter your Number");
+// var a = "0.231";
+// var num1 = parseInt(a)
+// var num1 = Number(a)
+// var num = 1 + num1; 
+// console.log(num)
+// function Greet(name){
+//     console.log("hi " + name)
+// }
+// Greet("musab")
+
+// Method of string 
+// var name = "E-COM BOOK STORE"
+// var name = "e-com book store"
+// var head = "E-com Book Store";
+// var head = head.toUpperCase();
+// var head = head.toLowerCase();
+// console.log(head)
+// console.log(city.length)
+// string index 0123456
+
+// var city = prompt("Enter your name") ;
+// var firstAlpha =  city.slice(0,1 ).toUpperCase();
+// var remainAlpha = city.slice(1,city.length).toLowerCase() 
+// console.log(firstAlpha+remainAlpha)
+// var text1 = "The New Yorker magazine doesn't allow the phrase''"
+// console.log(text1.length)
+//  for (var i = 0; i < text.length; i++) {
+    //      if (text.slice(i, i+12 ) === "World War II") {
+        //      text = text.slice(0, i) + "the Second world war" +text.slice(i + 12) ;
+        //      console.log(text)
+        //      }
+        //      }
+        // first instance
+//         var text = "The New Yorker magazine doesn't allow the phrase 'World War II' The New Yorker magazine doesn't allow the phrase 'World War II'"
+// var text = text.replace(/World War II/g,"the Second world war")
+// console.log(text)
+
+var text = "The New Yorker magazine doesn't allow the phrase 'World War II the'"
+// var num = text.length - 5
+// console.log(num)
+// var index = text.charAt(text.length - 5)
+// left to right indexof
+// right to left lastindexof
+var index = text.lastIndexOf("blue")
+console.log(index)
