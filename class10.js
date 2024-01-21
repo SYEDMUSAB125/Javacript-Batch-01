f4dee3eae848e26914b6c2768ddbd6ade5d4c606
Syed Musab <syedmasab125@gmail.com>
2024-01-21 19:42:18 +0500

    class10.js

---
+++ class10.js
@@ -0,0 +1,96 @@
+// var plan =  "basic";
+// var STUDENS = [arham , dayyan ,ahmed , ] 
+// var name = arham;
+// var nameColor = "purple"
+//  console.log(name )
+//  console.log(nameColor )
+//  var basic {
+    //     feeStatus: "paid",
+    //     laptop :true,
+    //     rollNo : "112",
+    //  }
+    //  var premium {
+        //     feeStatus: "paid",
+        //     laptop :true,
+        //     rollNo : "112",
+        //  }
+        //  var volunterr {
+            //     education : "'matric",
+            //     age : "23",
+            //     skills : "leader",
+            //  }
+            // objects
+//   var laptop = {
+//     name : "hp";
+//     gen : "core i5 4th generation",
+//     ram : "12gb"
+//   }
+//   var objectName = {
+//     key : value,
+  
+//  }
+// var name  = "musab"
+//   var plan1 = {
+//     // key : value 
+//      name: "Basic",
+//      price: 3.99,
+//      space: 100,
+//      transfer: 1000,
+//      pages: 10
+    //  };
+    //  var plan2 = {
+    //     // key : value 
+    //      name: "Basic",
+    //      price: 3.99,
+    //      space: 100,
+    //      transfer: 1000,
+    //      pages: 10
+    //      };
+         
+         //  console.log(name)
+        //  console.table(plan1)
+        //  console.log(plan2)
+        // Topic: nested object 
+        // function 
+        // Method: 
+// var resturant = {
+//     branch: 5,
+//     name : "gohar",
+//     exitDoor: 6,
+//     staff : 8,
+//     menu: ["karahi","chicken chowmein", "shashlick"],
+//     karahi :{
+//         spices:"shan",
+//         pani : "aquafina",
+//         verified : true
+//     } ,
+//     greet: function(){
+//         alert("assalam o alaikum")
+//     }
+ 
+
+// }
+// TASK1 : store your name chracter in array and reverse it through for loop
+// example Musab = basum
+// TASK2 : take input from user check the number is even or odd if even the allow to do browsing and odd dont allow statement is print.
+// for example: i pressed 2 print "you are allow to browsing" i presse 1 print " you are not allowed"
+
+
+
+var names = ["e","p","s","o","n"];
+
+for(i=4 ; i>=0; i--){
+
+  var name1 = name1 + names[i]  
+
+}
+var real = name1.slice(9,14)
+// console.log(real)
+document.write(real)
+
+
+
+// resturant.greet()
+// console.log(resturant.greet)
+// resturant.branch = 6;
+// resturant.name = "al sajjad"
