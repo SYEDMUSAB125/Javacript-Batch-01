e23159daa4000485a8ce26fa199c8325772de4cf
Syed Musab <syedmasab125@gmail.com>
2024-01-23 20:08:39 +0500

    updated class11.js

---
+++ class11.js
@@ -0,0 +1,37 @@
+//  var student1 = objCreater("musab", 56)
+//  var student2 = objCreater("mu", 56)
+//  var student3 = objCreater("musa", 56)
+//  var student4 = objCreater("mu", 56)
+//  var student5 = objCreater("msab", 56)
+//  var student6 = objCreater("usab", 56)
+//  console.log(student1)
+// var obj ={
+    
+    // }
+    // obj.name = "musab" ;
+    // obj.id = 56 
+    // console.log(obj)
+    // function ObjCreater(name1,rollno){ constructor 
+    //  var user = {}
+    //  user.name = name1;
+    //  user.id = rollno;
+    //  return user
+    // }
+    // ES6 
+// function Obj(name, id){
+//  this.name=name;
+//  this.id = id;
+
+// }
+// var student1 = new Obj("musab",67)
+// var date = new Date()
+// console.log(Date())
+// console.log(student1)
+
+
+
+
+
+
+ 
+
