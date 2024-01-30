54f0089c4872bbf9ba550eac7cc869d8f525c66b
Syed Musab <syedmasab125@gmail.com>
2024-01-30 22:21:30 +0500

    class13.js done

---
+++ class13.js
@@ -0,0 +1,43 @@
+// Es6 
+// Global vs local variable hoisting 
+// var a =10 
+// function sum(){
+//     var a = 15;  
+
+// }
+// sum()
+// console.log(a);
+// 15 
+// let a =5; 
+// function sum(){
+//     let  a =10; local
+// }
+// sum()
+// console.log(a) 10 
+// re-declare 
+// var a =5;
+// var a =10;
+// console.log(a)
+// not redeclare re-asign 
+// let b=5;
+// b =10;
+// console.log(b)
+// const b =10;
+// b=15; not re - assign 
+// let name1 = ["m","u","s","a","b"]
+// let result = name1.toString.split(" ")
+// console.log(name1);
+// let text = "Engineer Syed Musab"
+// const myArray = text.split(" ",1)
+// // const myArray = text.join(" ");
+// console.log(myArray)
+// pracice
+// split()
+// join()
+// repeat 
+
+
+
+
+
+
