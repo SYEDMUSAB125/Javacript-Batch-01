68c2c4e3f379aa1686b94bc5ed5e054e84cde018
SYED MUSAB <syedmasab125@gmail.com>
2024-02-25 00:32:51 +0500

    ecma script end

--- oops.js
+++ oops.js
@@ -32,6 +32,58 @@
 
 
 // console.log(person1,person2);
+// primitve value number boolean string 
+// var num = 1;
+// var num1 =  num
+// var num1= num1+1
+// console.log(num)
+// console.log(num1)
+// non-primitive value obj array
+// object  array
+// var obj1 = {
+//     name: "Ali",
+//     age: 20
+// };
+// var obj2 = obj1;
 
-
-
+// obj2.age = 30;
+// console.log(obj1,obj2);
+// var array = [1,2,3,4,5];
+// var array1 = array;
+// array1[0] = 100;
+// console.log(array,array1);
+// es6 class
+// class Person{
+//     constructor(name,age){
+//         this.name = name
+//         this.age = age
+//     }
+// }
+// let person1 = new person("musab",20)
+// inheritance children parent
+// class Men extends Person{
+   
+//     constructor(name, age, gender){
+//         super(name,age)
+//         this.gender = gender
+//     }
+// }
+// const men1 = new Men("musab",6,"male")
+// console.log(men1);
+// class Person {
+    // constructor(name,age){
+        //     this.name = name
+        //     this.age = age
+        // }
+        //   helper function
+//  static greeting(){
+//         console.log("Hello");
+//     }
+// }
+//  console.log(typeof(Person.greeting()))
+//  Date.now()
+// const men = new Person("musab",20)
+// console.log(typeof(men))
+localStorage.setItem("name","musab")
+let a = localStorage.getItem("name")
+console.log(a)
